09ef814648f105874d540e6d30255bec3f241a36
Naomi Fielder <NF230035@falmouth.ac.uk>
2022-01-29 02:19:58 +0000

    -Added some test waves to scene
    -Started to program - still not working fully but I am getting there

---
+++ Assets/Scripts/SCR_WaveManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SCR_WaveManager : MonoBehaviour
+{
+    public GameObject waveHolder;
+    private GameObject wave1;
+    private float objectY;
+    private bool cloneSpawned;
+
+    void Start()
+    {
+        cloneSpawned = false;
+        objectY = this.transform.position.y;
+        wave1 = this.gameObject;
+    }
+
+    void Update()
+    {
+        if (this.transform.position.x >= 0 & !cloneSpawned)
+        {
+            spawnWave();
+        }
+    }
+
+    void spawnWave()
+    {
+        Instantiate(wave1, new Vector2(-960f, objectY), Quaternion.identity, waveHolder.transform);
+        //wave1.transform.SetParent(waveHolder.transform);
+        cloneSpawned = true;
+
+    }
+}
